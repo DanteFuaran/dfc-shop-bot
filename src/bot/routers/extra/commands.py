4d3b0ca39ee94537dae496af92d8991e2e7ddbe6
DanteFuaran <DanteFuaran@users.noreply.github.com>
2026-01-23 21:46:03 +0100

    feat: remove /support command completely

--- src/bot/routers/extra/commands.py
+++ src/bot/routers/extra/commands.py
@@ -1,47 +1,6 @@
 from aiogram import Router
-from aiogram.filters import Command as FilterCommand
-from aiogram.types import Message
-from dishka import FromDishka
-from dishka.integrations.aiogram_dialog import inject
-from fluentogram import TranslatorRunner
-from loguru import logger
-import asyncio
-
-from src.core.config.app import AppConfig
-from src.core.enums import Command
-from src.core.utils.formatters import format_user_log as log
-from src.infrastructure.database.models.dto import UserDto
 
 router = Router(name=__name__)
-
-
-@inject
-@router.message(FilterCommand(Command.SUPPORT.value.command))
-async def on_support_command(
-    message: Message,
-    user: UserDto,
-    config: AppConfig,
-    i18n: FromDishka[TranslatorRunner],
-) -> None:
-    logger.info(f"{log(user)} Call 'support' command")
-
-    # Direct link to support bot
-    support_url = "tg://resolve?domain=test3_support_bot"
-
-    # Send deep link and delete immediately
-    response = await message.answer(support_url)
-    
-    # Delete both the command message and the response after short delay
-    try:
-        await asyncio.sleep(0.5)
-        await response.delete()
-    except Exception:
-        pass
-    
-    try:
-        await message.delete()
-    except Exception:
-        pass
 
 
 
